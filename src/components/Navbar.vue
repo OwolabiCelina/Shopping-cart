e7a3c1fda690f0de55ff7284eb9437a9801a473f
Owolabi Celina <108185066+OwolabiCelina@users.noreply.github.com>
2024-01-15 18:46:42 +0100

    Details-page added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -31,7 +31,7 @@
 
 <template>
     <div
-      class="bg-gray-900 text-white h-[5rem] p-4 md:flex md:items-center justify-between px-4"
+      class="bg-blue-900 text-white h-[5rem] p-4 md:flex md:items-center justify-between px-4"
     >
       <div class="logo text-2xl text-white font-bold cursor-pointer">
         <RouterLink to="/">Shopping Cart</RouterLink>
@@ -40,10 +40,10 @@
         <button class="bg-white text-black p-3 text-xl rounded-full" @click="toggleButton">{{ open ? "close" : "open" }}</button>
       </span>
       <ul
-        class="md:flex gap-5 items-center absolute md:static bg-gray-900 w-full md:w-auto pb-10 md:pb-0 px-6 md:px-0 top-14 duration-500 mt-5"
+        class="md:flex gap-5 items-center absolute md:static bg-blue-900 w-full md:w-auto pb-10 md:pb-0 px-6 md:px-0 top-14 mt-5"
         :class="[open ? 'left-0' : 'left-[-100%]']"
       >
-        <li v-for="tag in nav_links" class="py-4 md:my-0 hover:text-green-500 duration-300 text-xl">
+        <li v-for="tag in nav_links" class="py-4 md:my-0 duration-300 text-xl fw-500">
           <a href="#">{{ tag.name }}</a>
         </li>
       </ul>
@@ -58,7 +58,8 @@
   
   const nav_links = [
     { name: "Home" },
-    { name: "About" }
+    { name: "About" },
+    {name: "Cart"}
   ];
   
   const toggleButton = ()=>{
